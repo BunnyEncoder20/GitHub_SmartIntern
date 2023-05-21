95909d4ef250faea5cdeedae23e78f4784dec2bd
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2023-05-21 10:54:38 +0530

    completed DOM
    I still don't get it but it's used to get the input elements in the JS file

---
+++ JS/DOM.js
@@ -0,0 +1,40 @@
+/* 
+    DOM - Document Object Model 
+    --------------------------
+    it is API for html and XML documents .
+    All HTML elements in a page are put into one object called the DOM
+
+    DOM is a plateform and lannguage neutral interfadce where you can dynamically
+    access and update the content, style and structure of a html page 
+
+
+    Eg : An HTML page which has jsut one table : 
+    <table>
+    -> tr (multiple)
+        -> td (under each tr)
+            -> whatever tag under this 
+            
+
+    w3c Core parts
+    -------------
+    1. Core DOM : for all the documents types 
+    2. XML DOM : for only XML documents types 
+    3. HTML DOM : only for HTML documents types 
+    
+    DOM selectors 
+    ------------
+    document.getElementById()
+    document.querySelector
+    document.querySelectorAll
+
+
+    Events in JS
+    -----------
+    tells what to do when a specific 
+
+*/
+
+function printValue() {
+    var naam = document.getElementById("naam").value ;
+    alert(naam) ;
+}
