84e29287bee702feb9c9be860e7805b19f744908
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2023-05-22 22:58:28 +0530

    "Completed" JS today
    made the file which has the topics for the class, but not much actual code written init

---
+++ JS/22May.js
@@ -0,0 +1,45 @@
+/* 
+    Error handling in JS 
+    ------------------
+    try
+    catch 
+    throw 
+    finally 
+
+
+
+    try & catch 
+    ----------
+    try{
+        code that might cause error 
+    }
+    catch (error) 
+    {
+        code to handle that error 
+    }
+
+    
+
+    Throw
+    -------
+    normally want to stop & generate the error mesage and then we use throw
+    keyword to throw an exception.
+    Can be used with try and catch 
+
+
+    finally 
+    -------
+    try{
+
+    }
+    catch()
+    {
+
+    }
+    finally
+    {
+        execute this piece of code for sure if there is an exception or error, that type
+        of code should be present inside this block 
+    }
+
+*/
